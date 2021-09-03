9fe7c02377eaa77faa448a0e76ff64c6e6b6d017
Igor Pontes <46684536+igor-pontes@users.noreply.github.com>
2021-09-03 15:06:07 +0100

    Update continuation.py

--- continuation.py
+++ continuation.py
@@ -12,3 +12,41 @@ def add(v, x, y):
     return v+x+y
 
 print((lambda v, x=2, y=3: add(v,x,y))(1))
+
+def yes(info, no):
+    print(info)
+
+def no():
+    print("error")
+
+class Foo:
+    def __init__(self, info):
+        self._info = info
+
+    def check(self):
+        if self._info == "test":
+            return True
+        else:
+            return False
+
+    def search(self, yes, no):
+        if self.check():
+            yes(self._info, no)
+        else:
+            no()
+
+class Baz:
+    def __init__(self, foo1, foo2):
+        self.foo1 = foo1
+        self.foo2 = foo2
+
+    def search(self, yes, no):
+        return self.foo1.search(
+                    yes,
+                    lambda self=self, yes=yes, no=no: self.foo2.search(yes, no)
+                )
+
+foo1 = Foo("testd")
+foo2 = Foo("test")
+b = Baz(foo1, foo2)
+b.search(yes, no)
